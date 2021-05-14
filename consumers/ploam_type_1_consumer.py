967b086d2fb3034a08dee871d9973588030529d3
pancinator <matej.pancak@gmail.com>
2021-05-14 10:45:31 +0200

    Upravy producentov a konzumentov, odstranneie problemu s nevalidnym json, uprava threadovania vycistenie kodu

---
+++ consumers/ploam_type_1_consumer.py
@@ -0,0 +1,12 @@
+"""
+Example of kafka consumer subscribing topic PloamType1
+"""
+
+from kafka import KafkaConsumer
+from dp_kafka.src.kafka_services import config
+from json import loads
+import basic_consumer
+
+if __name__ == "__main__":
+    frame_prodcuer = basic_consumer.BasicConsumer(config.PLOAM_MESSAGES_BY_TYPE_1)
+    frame_prodcuer.print_messages()
---
+++ consumers/ploam_type_4_consumer.py
@@ -0,0 +1,10 @@
+"""
+Example of kafka consumer subscribing topic PloamType4
+"""
+
+from dp_kafka.src.kafka_services import config
+import basic_consumer
+
+if __name__ == "__main__":
+    frame_prodcuer = basic_consumer.BasicConsumer(config.PLOAM_MESSAGES_BY_TYPE_4)
+    frame_prodcuer.print_messages()
